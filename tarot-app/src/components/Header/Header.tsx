838f14b87528f078045a793df6e2f8672cfed64c
Pavel <p.strikhar@gmail.com>
2023-12-27 05:40:05 +0300

    tutu

--- tarot-app/src/components/Header/Header.tsx
+++ tarot-app/src/components/Header/Header.tsx
@@ -12,23 +12,18 @@ export function Header() {
     <div className="header">
       <div className="wrapper">
         <a className="header-item" href="http://google.com">
-          <HomeLogo className="logo" />
-          <span>Главная</span>
+          <TarotLogo className="logo" />
         </a>
         <a className="header-item" href="/gadanie">
-          <DivinationLogo className="logo" />
           <span>Гадания</span>
         </a>
         <a className="header-item" href="http://google.com">
-          <QuizLogo className="logo" />
           <span>Викторина</span>
         </a>
         <a className="header-item" href="http://google.com">
-          <TarotLogo className="logo" />
           <span>Значения</span>
         </a>
         <a className="header-item" href="http://google.com">
-          <CourseLogo className="logo" />
           <span>Курсы</span>
         </a>
       </div>
--- tarot-app/src/components/MainPage/MainPage.tsx
+++ tarot-app/src/components/MainPage/MainPage.tsx
@@ -1,10 +1,27 @@
 import { Header } from "../Header/Header";
 import "./MainPage.css";
+import { ReactComponent as TarotLogo } from "../../assets/pictures/tarot.svg";
 
 export function MainPage() {
   return (
     <div className="main">
       <Header></Header>
+      <div className="title-block">
+        <TarotLogo className="main-logo"></TarotLogo>
+        <h1 className="h1-main">Таро Онлайн</h1>
+        <span className="span-main">
+          Откройте дверь в мир тайн и предсказаний и отправьтесь в магическое
+          путешествие по разбору судьбы
+        </span>
+      </div>
+      <div className="title-block">
+        <TarotLogo className="main-logo"></TarotLogo>
+        <h1 className="h1-main">Таро Онлайн</h1>
+        <span className="span-main">
+          Откройте дверь в мир тайн и предсказаний и отправьтесь в магическое
+          путешествие по разбору судьбы
+        </span>
+      </div>
     </div>
   );
 }
